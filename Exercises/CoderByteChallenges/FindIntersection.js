e7f8b53f31ab3bac0cfdd0dda1c1e7b73459e143
DESKTOP-HSGUA6V\Anarchy <ziyanamli@gmail.com>
2021-11-25 14:05:11 +0300

    Coderbyte Bootcamp Hızlandırma Programı II 6.hafta challenge first promblem solved

--- Exercises/CoderByteChallenges/FindIntersection.js
+++ Exercises/CoderByteChallenges/FindIntersection.js
@@ -1,7 +1,24 @@
 function FindIntersection(strArr) { 
-
     // code goes here  
-    return strArr; 
+  
+    var firspart=strArr[0];
+    var secondpart=strArr[1];
+  
+    var firsArray=[];
+    var secondArray=[];
+  
+    firspart.split(",").map((anyNumber)=>{firsArray.push(Number(anyNumber))});
+    secondpart.split(",").map((anyNumber)=>{secondArray.push(Number(anyNumber))});
+  
+    var result= (secondArray.filter(e => firsArray.includes(e))).toString();
+  
+    if(!result){
+      return false;
+  
+    }
+    else{
+      return result;
+    }
   
   }
      
