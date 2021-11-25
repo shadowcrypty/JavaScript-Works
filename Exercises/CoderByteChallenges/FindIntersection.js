170fc93bdfc9763b70b12341708b5efea277e13c
DESKTOP-HSGUA6V\Anarchy <ziyanamli@gmail.com>
2021-11-25 13:34:31 +0300

    Coderbyte Bootcamp Hızlandırma Programı II 6.hafta challenge

---
+++ Exercises/CoderByteChallenges/FindIntersection.js
@@ -0,0 +1,9 @@
+function FindIntersection(strArr) { 
+
+    // code goes here  
+    return strArr; 
+  
+  }
+     
+  // keep this function call here 
+  console.log(FindIntersection(readline()));
