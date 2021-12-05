24cd6536fc8e6049cbb2da69b35373f8a9165e20
DESKTOP-HSGUA6V\Anarchy <ziyanamli@gmail.com>
2021-12-05 20:51:48 +0300

    nodemon , express and prettier usage exercise

---
+++ Exercises/PCAT/README.md
@@ -0,0 +1,15 @@
+# Nodemon , prettier and express package usage
+
+
+Nodemon install for use down below command
+```cmd
+npm install --save-dev nodemon
+```
+prettier install for use down below command
+```cmd
+npm i prettier
+```
+express install for use down below command
+```cmd
+npm i express
+```
---
+++ Exercises/PCAT/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  const photo = {
+    id: 1,
+    name: 'Photo name',
+    description: 'photo description',
+  };
+  res.send(photo);
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`${port} on local server live`);
+});
