15abe1cc5304557519485032e9bf6c242757933c
DESKTOP-HSGUA6V\Anarchy <ziyanamli@gmail.com>
2021-12-08 23:39:37 +0300

    ejs added in pcat project

--- Exercises/PCAT/app.js
+++ Exercises/PCAT/app.js
@@ -1,18 +1,22 @@
 const express = require('express');
 const path = require('path');
+const ejs = require('ejs');
 const app = express();
 
 const myLogger = (req, res, next) => {
   console.log('middleware');
   next();
 };
+//template engine
+app.set('view engine', 'ejs');
 
 //middlewares
 app.use(express.static('public'));
 app.use(myLogger);
 
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+  //res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+  res.render('index');
 });
 
 const port = 3000;
